0a1d093f821db3f75aca231d9ab0c6a36ef39756
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-10-25 08:46:49 -0400

    Add error handler, add header service.

--- src/app/components/category/category.service.ts
+++ src/app/components/category/category.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from "rxjs";
+import { EMPTY, Observable } from "rxjs";
 import { Category } from "./category.model";
+
+import { map, catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -17,25 +19,44 @@ export class CategoryService {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
-      panelClass: isError ? ['msg-error'] : ['msg-success']
+      panelClass: isError ? ["msg-error"] : ["msg-success"],
     });
   }
 
   create(category: Category): Observable<Category> {
-    return this.http.post<Category>(this.baseUrl, category);
+    return this.http.post<Category>(this.baseUrl, category).pipe(
+      map((obj:Category) => obj),
+      catchError((e:any) => this.errorHandler(e))
+    );
   }
 
   read(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.baseUrl);
+    return this.http.get<Category[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   readById(id: string): Observable<Category> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Category>(url);
+    return this.http.get<Category>(url).pipe(
+      map((obj:Category) => obj),
+      catchError((e:any) => this.errorHandler(e))
+    );
   }
 
   delete(id: string): Observable<Category> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Category>(url);
+    return this.http.delete<Category>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );;
+  }
+
+  errorHandler(e: any): Observable<any> {
+    console.log(e);
+    
+    this.showMessage("Ocorreu um erro!", true);
+    return EMPTY;
   }
 }
--- src/app/components/device/device.service.ts
+++ src/app/components/device/device.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from "rxjs";
 import { Device } from "./device.model";
+import { Observable,EMPTY } from "rxjs";
+import { map, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: "root",
@@ -22,20 +24,37 @@ export class DeviceService {
   }
 
   create(device: Device): Observable<Device> {
-    return this.http.post<Device>(this.baseUrl, device);
+    return this.http.post<Device>(this.baseUrl, device).pipe(
+      map((obj: Device) => obj),
+      catchError((e: any) => this.errorHandler(e))
+    );
   }
 
   read(): Observable<Device[]> {
-    return this.http.get<Device[]>(this.baseUrl);
+    return this.http.get<Device[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   readById(id: string): Observable<Device> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Device>(url);
+    return this.http.get<Device>(url).pipe(
+      map((obj:Device) => obj),
+      catchError((e:any) => this.errorHandler(e))
+    );
   }
 
   delete(id: string): Observable<Device> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Device>(url);
+    return this.http.delete<Device>(url).pipe(
+      map((obj:Device) => obj),
+      catchError((e:any) => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e:any):Observable<any> {
+    this.showMessage("Ocorreu um erro", true);
+    return EMPTY;
   }
 }
